636ac62604f993f29ed2ec7444b143a57820149a
Julia MJ <129623927+juliamj222@users.noreply.github.com>
2024-02-27 19:19:56 -0500

    routes

--- src/App.js
+++ src/App.js
@@ -35,6 +35,7 @@ function App() {
         <Route path="/math-games" element={<MathGames />} />
         <Route path="/pokedex" element={<Pokedex />} />
       </Routes>
+
       <Footer />
     </div>
   );
